186343bfd7d8f0fa528d6ff2c409275d4c0735c4
NettoJM <nettojm86@gmail.com>
2023-07-17 15:41:35 -0300

    Implement text_importer method to read and return a list of the file lines

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,21 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+
+    # Verifica se o arquivo tem a extensão correta
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return None
+
+    try:
+        # Abre o arquivo para leitura
+        with open(path_file, 'r') as file:
+            # Lê as linhas do arquivo e retorna uma lista
+            lines = file.read().split('\n')
+            return lines
+
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        return None
