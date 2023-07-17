fe252b31679d83e492fc0e1f3573492554e5aed6
NettoJM <nettojm86@gmail.com>
2023-07-17 17:15:36 -0300

    Implement process function and helper methods

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,28 @@
+from ting_file_management.file_management import txt_importer
+
+
+def is_valid_path(path_file, instance):
+    for dict in instance.items:
+        if dict.get("nome_do_arquivo") == path_file:
+            return False
+    return True
+
+
+def get_dict_data(lines, path_file):
+    dict = {}
+    dict["nome_do_arquivo"] = path_file
+    dict["qtd_linhas"] = len(lines)
+    dict["linhas_do_arquivo"] = lines
+    return dict
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    if is_valid_path(path_file, instance):
+        lines = txt_importer(path_file)
+        dict_data = get_dict_data(lines, path_file)
+        instance.enqueue(dict_data)
+        print(dict_data)
 
 
 def remove(instance):
